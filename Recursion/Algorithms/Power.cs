8ea444ed6a021a4dfb0daa4747d5044a4e856426
Aaron <aarlin5409@hessleacademy.com>
2020-11-12 14:22:49 +0000

    added power function

---
+++ Recursion/Algorithms/Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursion
+{
+    public static partial class Algorithms
+    {
+        public static int Power(int y, int n)
+        {
+            if (n < 1)
+                throw new ArgumentException("Exponent cannot be below 1");
+
+            //base case
+            if (n == 1)
+                return y;
+
+            return y * Power(y, n - 1);
+        }
+    }
+}
---
+++ RecursionTests/Algorithms/PowerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recursion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursion.Tests
+{
+    [TestClass()]
+    public class PowerTests
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, 1)]
+        [DataRow(2, 3, 8)]
+        [DataRow(7, 8, 5764801)]
+        public void PowerTest(int y, int n, int expected)
+        {
+            int power = Algorithms.Power(y, n);
+
+            Assert.AreEqual(expected, power);
+        }
+
+        [DataTestMethod]
+        [DataRow(12, 0)]
+        [DataRow(4, -4231)]
+        public void PowerTestError(int y, int n)
+        {
+            Assert.ThrowsException<ArgumentException>(() => Algorithms.Power(y, n));
+        }
+    }
+}
